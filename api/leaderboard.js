57c30907ab633d59cca1f28d9de8df0b3f0b0b24
EgrBiznes <kh.egor18@ya.ru>
2025-11-28 20:59:40 +0400

    leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,1 +1,11 @@
+let leaderboard = [];
 
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // Возвращаем топ-10 игроков
+    const sorted = [...leaderboard].sort((a, b) => b.balance - a.balance);
+    res.status(200).json({ leaderboard: sorted.slice(0, 10) });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
